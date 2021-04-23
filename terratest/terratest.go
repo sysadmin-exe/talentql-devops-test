67ee1ed23e0018e792174d2586ab2bb466585f96
Victor Efedi <victorefedi@Victors-MacBook-Pro.local>
2021-04-23 17:13:26 +0100

    terratest

--- terratest/terratest.go
+++ terratest/terratest.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
-
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-07-01/compute"
 	"github.com/gruntwork-io/terratest/modules/azure"
