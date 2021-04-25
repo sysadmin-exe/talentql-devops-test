1894096055fdf9adeab778481ecb3551b49c5d3b
Victor Efedi <victorefedi@Victors-MacBook-Pro.local>
2021-04-25 22:15:07 +0100

    renaming terraform dir name

--- terratest/terratest.go
+++ terratest/terratest.go
@@ -21,7 +21,7 @@ func TestTerraformApacheserver(t *testing.T) {
 	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// The path to where our Terraform code is located
-		TerraformDir: "../terraform/",
+		TerraformDir: "../tf/",
 	})
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created.
