c89b418347036e9c859ddf5ea7beb00c3ada2ba0
Victor Efedi <victorefedi@Victors-MacBook-Pro.local>
2021-04-25 23:40:53 +0100

    added terratest and all its files

--- tf/variable.tf
+++ tf/variable.tf
@@ -47,7 +47,7 @@ variable "tags" {
 variable "location" {
   description = "Location of the resource group"
   type        = string
-  default     = "East US"
+  default     = "West Europe"
 }
 
 # Vnet and Subnet
