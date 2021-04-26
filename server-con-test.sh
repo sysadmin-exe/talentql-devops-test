bc59d1936d06181786355b557e48c9e3f473ff89
Victor Efedi <victorefedi@Victors-MacBook-Pro.local>
2021-04-26 23:59:12 +0100

    testing bash script-final tests for script

--- server-con-test.sh
+++ server-con-test.sh
@@ -6,16 +6,21 @@ terraform -chdir=./tf/ apply --auto-approve > output
 
 # test the connectivity to the server IP address on port 80
 export URL=$(grep public_ip_address output | awk '{print $3}' | sed 's/"//g')
-echo "The IP address of the server is $URL"
+echo ">>>>>>>>>>>>>>>>>>>> INFO : The IP address of the server is $URL <<<<<<"
 
 status_code=$(curl --write-out %{http_code} --silent --output /dev/null -m 10 $URL:80)
 
-echo "The server gives a $status_code status code"
+echo ">>>>>>>>>>>>>>>>>>>> INFO: The server gives a $status_code status code <<<<<<"
 
 if [[ "$status_code" == 200 ]] ; then
-  echo "Terraform deployment works! Apache Server is up!!" 
+  echo "##########################################################"
+  echo "## Terraform deployment works! Apache Server is up!! :) ##"
+  echo "##########################################################"
 else
-  echo "Terraform deployment is wrong! Apache Server is Down!!" 
+  echo "###############################################################"
+  echo "## Terraform deployment is wrong! Apache Server is Down!! :( ##" 
+  echo "###############################################################"
+
   exit 1
 fi
 
