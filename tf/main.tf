5c8d38dcf596e049701a8fe6f881ba4eb4e67253
Victor Efedi <victorefedi@Victors-MacBook-Pro-2.local>
2021-07-09 23:06:35 +0100

    changed backend config

--- tf/main.tf
+++ tf/main.tf
@@ -17,7 +17,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "remote-state"
     storage_account_name = "terraformlearn0702"
-    container_name       = "statefilecontainer3"
+    container_name       = "statefilecontainer4"
     key                  = "terraform.tfstate"
   }
 }
